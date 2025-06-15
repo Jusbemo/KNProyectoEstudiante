8793b14f62b071b331dd964f62bc7984a0a01079
Justin Berger <jusaleberger@gmail.com>
2025-06-15 16:16:31 -0600

    Semana 5
    Avance Semana 5, usando Linq, y SP's con la DB

--- KProyecto/Controllers/HomeController.cs
+++ KProyecto/Controllers/HomeController.cs
@@ -1,14 +1,83 @@
-﻿using System.Web.Mvc;
+﻿using KProyecto.EF;
+using KProyecto.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace KProyecto.Controllers
 {
     public class HomeController : Controller
     {
+
+        #region Index
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(Autenticacion autenticacion)
+        {
+            using (var dbContext = new KNDataBaseEntities1())
+            {
+                //var result = dbContext.TUsuario.FirstOrDefault(u => u.NombreUsuario == autenticacion.NombreUsuario
+                //                                                 && u.Contrasenna == autenticacion.Contrasenna);
+
+                var result = dbContext.ValidarInicioSesion(
+                    autenticacion.NombreUsuario,
+                    autenticacion.Contrasenna).FirstOrDefault();
+
+                if (result != null)
+                    return RedirectToAction("Principal", "Home");
+
+                ViewBag.Mensaje = "No se pudo validar su información";
+                return View();
+            }
+        }
+
+        #endregion
+
+        #region Registro
+
+        [HttpGet]
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registro(Autenticacion autenticacion)
+        {
+            using (var dbContext = new KNDataBaseEntities1())
+            {
+                //dbContext.TUsuario.Add(new TUsuario
+                //{
+                //    Nombre = autenticacion.Nombre,
+                //    Correo = autenticacion.Correo,
+                //    NombreUsuario = autenticacion.NombreUsuario,
+                //    Contrasenna = autenticacion.Contrasenna
+                //});
+
+                //var result = dbContext.SaveChanges();
+
+                var result = dbContext.RegistroUsuario(
+                    autenticacion.Nombre,
+                    autenticacion.Correo,
+                    autenticacion.NombreUsuario,
+                    autenticacion.Contrasenna);
+
+                if (result > 0)
+                    return RedirectToAction("Index", "Home");
+
+                ViewBag.Mensaje = "No se pudo registrar su información";
+                return View();
+            }
+        }
+
+        #endregion
+
+        [HttpGet]
         public ActionResult Principal()
         {
             return View();
---
+++ KProyecto/Models/Autenticacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KProyecto.Models
+{
+	public class Autenticacion
+	{
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contrasenna { get; set; } 
+    }
+}
